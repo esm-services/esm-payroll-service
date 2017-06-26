293d205c0c259e9b1a88b44ce58970e83440b126
Durgesh RAI <drai548@gmail.com>
2017-06-26 16:14:08 +0530

    initial commit

---
+++ src/main/java/com/esm/payroll/service/PayrollServiceApplication.java
@@ -0,0 +1,12 @@
+package com.esm.payroll.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollServiceApplication.class, args);
+	}
+}
