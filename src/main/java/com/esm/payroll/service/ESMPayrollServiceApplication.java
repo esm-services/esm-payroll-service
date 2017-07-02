439a466d8bad0a4618ef1d32b769b9cb55f54eb0
Durgesh RAI <drai548@gmail.com>
2017-07-02 22:06:39 +0530

    renamed the main class

--- src/main/java/com/esm/payroll/service/ESMPayrollServiceApplication.java
+++ src/main/java/com/esm/payroll/service/ESMPayrollServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayrollServiceApplication {
+public class ESMPayrollServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PayrollServiceApplication.class, args);
+		SpringApplication.run(ESMPayrollServiceApplication.class, args);
 	}
 }
--- src/test/java/com/esm/payroll/service/ESMPayrollServiceApplicationTests.java
+++ src/test/java/com/esm/payroll/service/ESMPayrollServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PayrollServiceApplicationTests {
+public class ESMPayrollServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
